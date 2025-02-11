6b1e5ba6f10e04dbeed2be50a90362b8c93656dd
Marcian2 <suta.marcian@yahoo.com>
2025-02-11 03:35:27 +0200

    Am facut-o si pe asta

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, {Express} from 'express';
 interface RSP {
   message: string;
 }
+
 interface Gym {
   id: number;
   name: string;
@@ -31,6 +32,7 @@ app.route('/gyms').get((req, res) => {
     payload:gyms //cand trimiti info mai mult decat un mesaj
   })
 })
+
 
 app.route('/gyms/:id').get((req, res) => {
   const id= Number(req.params?.id);
@@ -94,6 +96,111 @@ app.route('/gyms/:id').delete((req, res) => {
 
     }
   })
+})
+
+
+
+app.route('/gyms/').post((req,res)=>{
+  const {id, name, address} = req.body;
+  //check the id to be a number
+  if(!id || !name || !address) {
+    res.status(400).send({
+      message: 'Insert correct id, name and adress.'
+    } as RSP);
+
+    return;
+  }
+
+  //check id existence
+  const index=gyms.findIndex((gym:Gym)=> gym.id===id);
+  if (index) {
+    res.status(400).send({
+      message: `A gym with id ${id} already exists. Please use a unique id.`
+    } as RSP);
+    return;
+  }
+
+  const newGym:Gym = {id, name, address };
+  gyms.push(newGym);
+
+  res.status(200).send({
+    message:'Gym added succesfully'
+  }as RSP);
+
+})
+
+//update la o sala, indexul nu il schimb ca nu vreau 
+app.route('/gyms/').put((req,res)=>{
+    'Insert the gym s id you want to update and the new name and adress' 
+    const {id,name,address}=req.body;
+    if(!id || !name || !address) {
+      res.status(400).send({
+        message: 'Insert correct id, name and adress.'
+      } as RSP);
+  
+      return;
+    }
+
+    const index=gyms.findIndex((gym:Gym)=> gym.id=id);
+    
+    if(index) {
+      gyms[index].name=name;
+      gyms[index].address=address;
+      res.status(200).send({
+        message:'Gym updated succesfully'
+      }as RSP);
+    }
+    else
+    {
+      res.status(400).send({
+        message: 'Index not found in the gyms.'
+      } as RSP);
+      return;
+    }
+
+})
+
+app.route('/gyms/').patch((req,res)=>{
+  "Insert id of the desired gym you want to patch"
+  const id = Number(req.params);
+  //check the index etc 
+  if(!id)
+  {res.status(400).send({
+    message: 'Id should be a number'
+  } as RSP);
+
+  return;
+  }
+
+  const index=gyms.findIndex((gym:Gym)=> gym.id=id)
+  
+  if(index){
+      "Input what do you want to change: 1 for name and 2 for adress accordingly" 
+      //aici for the moment las asa cum fac aici desi trebuie putin(foarte tare) modificat ulterior
+      const number =Number(req.params);
+      if(number === 1){
+      "Introdu numele salii"
+        const change=String(req.params);
+        gyms[index].name=change;
+        res.status(200).send({
+          message:"Super, s-a dat patch la nume"
+        }as RSP)  
+      return;}
+      else if(number === 2){
+        "introdu adresa salii"
+        const change=String(req.params);
+        gyms[index].address=change;
+        res.status(200).send({
+          message:"Si mai super, s-a mutat sala"
+        }as RSP)
+      }
+    }
+    else{
+      res.status(400).send({
+        message: 'aparent nu exista indexul in salile existente'
+      }as RSP)
+    return;}
+
 })
 
 app.listen(port, () => {
